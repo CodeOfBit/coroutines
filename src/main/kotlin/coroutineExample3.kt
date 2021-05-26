a927f1f9cfdbcc1aabb83211f608131a0662da51
arka <arkamondal1995@outlook.com>
2021-05-26 20:17:33 +0530

    MAY-26/2021/8.17PM

---
+++ src/main/kotlin/coroutineExample3.kt
@@ -0,0 +1,20 @@
+import kotlinx.coroutines.*
+
+/*
+*  ********************************* Note ***********************************
+*   Hare we used coroutine scope function and join and job function
+*
+*
+*/
+fun main()= runBlocking{ // Create a blocking coroutine that executes in current thread (main)
+
+    println("Main program starts: ${Thread.currentThread().name}") // Main Thread
+    val job : Job = launch { // Thread T1
+        println("Fake work stats: ${Thread.currentThread().name}") // Thread T1
+        delay(1000)  // Coroutine is suspended but Theread : T1 is free (not blocking)
+        println("Fake work finished: ${Thread.currentThread().name}") // Either T1 or some other thread
+    }
+
+    job.join()
+    println("Main program ends: ${Thread.currentThread().name}") // Complete the amin Thread
+}
